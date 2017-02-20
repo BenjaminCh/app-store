3d0d353c4f8f1726433697f7eb5619e895d9052a
BenjaminCh <benjamin.chastanier@gmail.com>
2017-02-20 14:29:18 +0100

    [Backend] [Config] Allow a default value in get config functions #5

--- backend/interfaces/configuration_interface.go
+++ backend/interfaces/configuration_interface.go
@@ -13,3 +13,27 @@ type IConfiguration interface {
 type ConfigurationManager struct {
 	ConfigurationInteractor IConfiguration
 }
+
+// GetConfig allows to retrieve a configuration value for a given key.
+// Implements function from IConfiguration interface
+func (c ConfigurationManager) GetConfig(key string, defaultValue interface{}) interface{} {
+	return c.ConfigurationInteractor.GetConfig(key, defaultValue)
+}
+
+// GetConfigString allows to retrieve a configuration String for a given key.
+// Implements function from IConfiguration interface
+func (c ConfigurationManager) GetConfigString(key string, defaultValue string) string {
+	return c.ConfigurationInteractor.GetConfig(key, defaultValue).(string)
+}
+
+// GetConfigBool allows to retrieve a configuration Boolean for a given key.
+// Implements function from IConfiguration interface
+func (c ConfigurationManager) GetConfigBool(key string, defaultValue bool) bool {
+	return c.ConfigurationInteractor.GetConfig(key, defaultValue).(bool)
+}
+
+// GetConfigUInt allows to retrieve a configuration UInt for a given key.
+// Implements function from IConfiguration interface
+func (c ConfigurationManager) GetConfigUInt(key string, defaultValue uint) uint {
+	return c.ConfigurationInteractor.GetConfig(key, defaultValue).(uint)
+}
